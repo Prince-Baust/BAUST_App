e4713f2e84db1454b7dd2e2c12cfae50cc7edabd
pmaruf42 <pmaruf42@gmail.com>
2018-07-10 15:55:47 +0600

    Added scroll view, rate_app & share_app

--- app/src/main/java/com/geek/baust_app/MainActivity.java
+++ app/src/main/java/com/geek/baust_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.geek.baust_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_CSE, btn_EEE, btn_ME;
+    Button btn_CSE, btn_EEE, btn_ME, btn_rate, btn_share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         btn_CSE = findViewById(R.id.btn_cse);
         btn_EEE = findViewById(R.id.btn_eee);
         btn_ME = findViewById(R.id.btn_me);
+        btn_rate = findViewById(R.id.btn_rateApp);
+        btn_share = findViewById(R.id.btn_shareApp);
 
         btn_CSE.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Mechanical Engineering", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, MeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/dev?id=5700313618786177705"));
+                startActivity(intent);
+            }
+        });
+
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "Download the app from http://www.exmple.com");
+                intent.setType("text/plain");
                 startActivity(intent);
             }
         });
